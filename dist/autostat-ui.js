17de34f5241a28586245efd9deb7c01c128a85b9
Nikola Lazarov <nnl160@aubg.edu>
2020-03-08 09:18:57 +0200

    add sidebar scripts

--- dist/autostat-ui.js
+++ dist/autostat-ui.js
@@ -28,3 +28,17 @@
   hamburger.addEventListener('click', hamburgerHandler);
   window.addEventListener('resize', closeHamburger);
 })();
+(function() {
+  const sidebars = document.querySelectorAll('.Sidebar');
+
+  sidebars.forEach(sidebar => {
+    const listItems = Array.from(sidebar.querySelectorAll('li'));
+    const links = Array.from(sidebar.querySelectorAll('a'));
+
+    links.forEach((link, index) => {
+      const href = link.getAttribute('href');
+      const targetUrl = new URL(href, document.baseURI).href;
+      if (targetUrl === document.url) listItems[index].classList.add('is-active');
+    });
+  });
+})();
