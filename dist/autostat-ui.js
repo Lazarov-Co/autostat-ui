8b2253b6cc45631e144d9d1a966d0deb72b2f980
Nikola Lazarov <nnl160@aubg.edu>
2020-04-05 16:29:43 +0300

    add card header

--- dist/autostat-ui.js
+++ dist/autostat-ui.js
@@ -1,4 +1,58 @@
 (function() {
+  const dialogs = document.querySelectorAll('.js-dialog');
+  const openDialogTriggers = document.querySelectorAll('.js-open-dialog');
+  const closeDialogTriggers = document.querySelectorAll('.js-close-dialog');
+
+  dialogs.forEach(dialog => {
+    const overlay = document.createElement('div');
+    overlay.classList.add('DialogOverlay', 'js-dialog-overlay');
+    overlay.addEventListener('click', () => close(dialog));
+    dialog.parentElement.insertBefore(overlay, dialog);
+    
+    dialog.autostat_ui = {
+      overlay,
+      close: function() {
+        close(dialog);
+      },
+      open: function() {
+        open(dialog);
+      }
+    }
+  });
+
+  function open(dialog) {
+    document.body.style.overflow = 'hidden';
+    dialog.autostat_ui.overlay.style.display = 'block';
+    dialog.style.display = 'block';
+  }
+
+  function close(dialog) {
+    document.body.style.overflow = 'auto';
+    dialog.autostat_ui.overlay.style.display = 'none';
+    dialog.style.display = 'none';
+  }
+
+  function openDialogTriggerHandler(trigger) {
+    const id = trigger.getAttribute('data-dialog-id');
+    const dialog = document.querySelector(`#${id}`);
+    open(dialog);
+  }
+
+  function closeDialogTriggerHandler(trigger) {
+    const id = trigger.getAttribute('data-dialog-id');
+    const dialog = document.querySelector(`#${id}`);
+    close(dialog);
+  }
+
+  openDialogTriggers.forEach(trigger => {
+    trigger.addEventListener('click', () => openDialogTriggerHandler(trigger));
+  });
+
+  closeDialogTriggers.forEach(trigger => {
+    trigger.addEventListener('click', () => closeDialogTriggerHandler(trigger));
+  });
+})();
+(function() {
   const header = document.querySelector('.Header');
   if (header === null) return;
 
